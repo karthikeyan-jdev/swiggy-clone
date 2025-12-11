cc71cf2c04bb4fbaeb2d918da2829e5f4765535a
karthikeyan.jdev <karthikeyan.jdev@gmail.com>
2025-12-11 12:18:24 +0530

    test

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,18 +1,45 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import Home from "../pages/Home";
+import AboutUs from "../pages/AboutUs";
+import Cart from "../pages/Cart";
+import Favories from "../pages/Favories";
+import Login from "../pages/Login";
+import Signin from "../pages/Signin";
 
 const swiggyRouter = createBrowserRouter([
-    {
-        path:"/",
-        element:<MainLayout/>,
-        children:[{
-            index:true,
-            element:<Home/>
-        }]
-    }
-])
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "AboutUs",
+        element: <AboutUs />,
+      },
+      {
+        path: "Cart",
+        element: <Cart />,
+      },
+      {
+        path: "Favories",
+        element: <Favories />,
+      },
+      {
+        path: "Login",
+        element: <Login />,
+      },
+      {
+        path: "Signin",
+        element: <Signin />,
+      },
+    ],
+  },
+]);
 
-export default function AppProvider(){
-    return <RouterProvider router={swiggyRouter}/>
+export default function AppProvider() {
+  return <RouterProvider router={swiggyRouter} />;
 }
