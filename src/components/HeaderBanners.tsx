f6d4e9b8da97de0b8f4e57459c9d6d964fef8bde
karthikeyan.jdev <karthikeyan.jdev@gmail.com>
2025-12-12 21:03:06 +0530

    update

--- src/components/HeaderBanners.tsx
+++ src/components/HeaderBanners.tsx
@@ -39,47 +39,45 @@ const HeaderBanners = () => {
           <img
             src="https://static.vecteezy.com/system/resources/thumbnails/070/116/360/small/delicious-chicken-biryani-with-garnish-in-white-bowl-on-transparent-background-png.png"
             alt="deliveryPerson"
-            className="w-44 h-45 absolute right-[-30px] top-[px]"
+            className="w-44 h-45 absolute right-[-30px]"
           />
         </div>
         <div className="relative overflow-hidden bg-white rounded-xl  ">
-          <div className="flex flex-col justify-center  ">
-            <div className="px-4 pt-4 space-y-0.5">
-              <h4 className="font-bold text-[18px] text-gray-600 ">
-                INSTANMART
-              </h4>
-              <p className="text-gray-500 text-[12px] min-w-max">THE EVERYTHINGS STORE</p>
-              <p className="text-primary font-semibold text-[12px] px-1">
-                FREE DELIVERY
-              </p>
-            </div>
+          <div className="px-4 pt-4 space-y-0.5">
+            <h4 className="font-bold text-[18px] text-gray-600 ">INSTANMART</h4>
+            <p className="text-gray-500 text-[12px] min-w-max">
+              THE EVERYTHINGS STORE
+            </p>
+            <p className="text-primary font-semibold text-[12px] px-1">
+              FREE DELIVERY
+            </p>
+          </div>
 
-            <div className="">
-              <img
-                src="https://static.vecteezy.com/system/resources/thumbnails/054/591/691/small/3d-render-of-cosmetics-products-free-png.png"
-                alt=""
-                className="w-42 h-35 absolute right-[-19px] top-[78px]"
-              />
-            </div>
+          <div className="">
+            <img
+              src="https://static.vecteezy.com/system/resources/thumbnails/054/591/691/small/3d-render-of-cosmetics-products-free-png.png"
+              alt=""
+              className="w-42 h-35 absolute right-[-19px] top-[78px]"
+            />
           </div>
         </div>
         <div className="relative overflow-hidden bg-white rounded-xl  ">
-          <div className="flex flex-col justify-center ">
-            <div className="px-4 pt-4 space-y-0.5">
-              <h4 className="font-bold text-[18px] text-gray-600 ">DINEOUT</h4>
-              <p className="text-gray-500 text-[12px] min-w-max">EAT OUT & SAVE MORE</p>
-              <p className="text-primary font-semibold text-[12px] px-1">
-                UP TO 50% OFF
-              </p>
-            </div>
+          <div className="px-4 pt-4 space-y-0.5">
+            <h4 className="font-bold text-[18px] text-gray-600 ">DINEOUT</h4>
+            <p className="text-gray-500 text-[12px] min-w-max">
+              EAT OUT & SAVE MORE
+            </p>
+            <p className="text-primary font-semibold text-[12px] px-1">
+              UP TO 50% OFF
+            </p>
+          </div>
 
-            <div className="">
-              <img
-                src="https://static.vecteezy.com/system/resources/thumbnails/053/204/993/small/cutlery-on-plates-with-beige-napkin-on-transparent-background-free-png.png"
-                alt=""
-                className="w-40 h-30 relative right-[-83px] top-[20px]"
-              />
-            </div>
+          <div className="">
+            <img
+              src="https://static.vecteezy.com/system/resources/thumbnails/053/204/993/small/cutlery-on-plates-with-beige-napkin-on-transparent-background-free-png.png"
+              alt=""
+              className="w-40 h-30 relative left-[45px] top-[22px]"
+            />
           </div>
         </div>
       </div>
